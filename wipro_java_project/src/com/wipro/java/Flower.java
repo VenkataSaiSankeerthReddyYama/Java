545fefde7881d675774fc497074d04d0803c5c8a
VenkataSaiSankeerthReddyYama <sankeerthreddy2002@gmail.com>
2025-02-01 16:04:41 +0530

    day1

---
+++ wipro_java_project/src/com/wipro/java/Flower.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.wipro.java;
+
+/**
+ * @author Sankeerth reddy
+ *
+ */
+public class Flower {
+	//properities and behaviours
+	//name, color
+	//rose, red
+	//lotus, pink
+	
+	//member variables
+	String nameValue;//data type-->string
+	String colorValue;//data type--> string
+     
+	//constructor
+	Flower(String name, String color) {
+		this.nameValue=name;
+		this.colorValue=color;
+		
+	}
+	//method--> combination of propeerities & nehaviours
+	void display() {
+		System.out.println("Flower name:"+nameValue+"Flower color"+colorValue);
+	}
+
+//main-- method	
+	public static void main(String args[]) {
+		// Initializing color strings directly
+        String red = "red";
+        String pink = "pink";
+		//object is instance of a Class
+		//rose is a instance of flower
+		//rose is type of flower
+		//Class objectname=new Constructor(parameters)
+		Flower flowerobject1=new Flower("rose", red);
+		Flower flowerobject2=new Flower("lotus", pink);
+		flowerobject1.display();
+		flowerobject2.display();
+		
+	}
+
+}
