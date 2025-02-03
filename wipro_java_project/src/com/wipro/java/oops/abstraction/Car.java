314f09a8ef39a5cfa530c5da11949fe506acf1e4
VenkataSaiSankeerthReddyYama <sankeerthreddy2002@gmail.com>
2025-02-03 16:06:09 +0530

    day2

---
+++ wipro_java_project/src/com/wipro/java/oops/abstraction/Car.java
@@ -0,0 +1,10 @@
+package com.wipro.java.oops.abstraction;
+
+public abstract class Car {
+
+    // Abstract method to be implemented by child classes
+    abstract void mileage();
+
+    // Abstract method to be implemented by child classes
+    abstract void speed();
+}
