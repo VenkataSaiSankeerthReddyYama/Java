f59de1e04459758ce390362851d8ee5051f19aa3
VenkataSaiSankeerthReddyYama <sankeerthreddy2002@gmail.com>
2025-02-03 11:27:19 +0530

    day2

--- wipro_java_project/src/com/wipro/java/oops/Employee.java
+++ wipro_java_project/src/com/wipro/java/oops/Employee.java
@@ -1,9 +1,52 @@
 package com.wipro.java.oops;
 
+/**
+ * @author Sankeerth reddy
+ *
+ */
 public class Employee {
+    private int employee_no;
+    private String name;
+    private String emailID;
+    private float salary; 
 
-	public Employee() {
-		// TODO Auto-generated constructor stub
-	}
+    // Getter for employee_no
+    public int getEmployee_no() {
+        return employee_no;
+    }
 
+    // Setter for employee_no
+    public void setEmployee_no(int employee_no) {
+        this.employee_no = employee_no;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for emailID
+    public String getEmailID() {
+        return emailID;
+    }
+
+    // Setter for emailID
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    // Getter for salary
+    public float getSalary() {
+        return salary;
+    }
+
+    // Setter for salary
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
 }
