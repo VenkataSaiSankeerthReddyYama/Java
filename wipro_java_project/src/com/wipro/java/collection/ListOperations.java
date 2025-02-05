e24f279ef765b58c95f99971ed9c56a289f6d98d
VenkataSaiSankeerthReddyYama <sankeerthreddy2002@gmail.com>
2025-02-05 14:34:36 +0530

    day4

--- wipro_java_project/src/com/wipro/java/collection/ListOperations.java
+++ wipro_java_project/src/com/wipro/java/collection/ListOperations.java
@@ -6,9 +6,9 @@ public class ListOperations {
 	
 	public static void main(String[] args) {
 		
-		//vomplemented by the ArrayList Class
+		//impplemented the ArrayList Class
 		List<Integer> l1 = new ArrayList<Integer>();
-		
+		// add to list
 		l1.add(0, 1);
 		l1.add(1, 2);
 		l1.add(2, 2);
@@ -16,7 +16,7 @@ public class ListOperations {
 		System.out.println(l1);
 		
 		List<Integer> l2 =  new ArrayList<Integer>();
-		
+		//add to list 2
 		l2.add(1);
 		l2.add(2);
 		l2.add(3);
@@ -24,7 +24,7 @@ public class ListOperations {
 		l1.addAll(1, l2);
 		
 		System.out.println(l1);
-		
+	//removes element from index
 		l1.remove(1);
 		
 		System.out.println(l1);
