9344afb772d11c7ee42443f1755d4164eaacac48
VenkataSaiSankeerthReddyYama <sankeerthreddy2002@gmail.com>
2025-02-06 11:24:38 +0530

    Day5

--- wipro_java_project/src/com/wipro/java/collection/Movie.java
+++ wipro_java_project/src/com/wipro/java/collection/Movie.java
@@ -1,53 +1,34 @@
 package com.wipro.java.collection;
 
-import java.util.ArrayList;
-import java.util.Collections;
+public class Movie {
+    private String name;
+    private double rating;
+    private int year;
 
-public class Movie implements Comparable<Movie> {
-    private String n;
-    private double r;
-    private int y;
-
-    public Movie(String n, double r, int y) {
-        this.n = n;
-        this.r = r;
-        this.y = y;
+    // Constructor
+    public Movie(String name, double rating, int year) {
+        this.name = name;
+        this.rating = rating;
+        this.year = year;
     }
 
-    // Implementing the compareTo method to compare movies by year
-    @Override
-    public int compareTo(Movie m) {
-        return this.y - m.y; // Compare based on the year
-    }
-
-    // Correcting the method name typo
+    // Getters
     public String getName() {
-        return n;
+        return name;
     }
 
     public double getRating() {
-        return r;
+        return rating;
     }
 
-    // Adding the getYear method
     public int getYear() {
-        return y;
+        return year;
     }
 
-    public static void main(String[] args) {
-        // Creating the list of Movie objects
-        ArrayList<Movie> l = new ArrayList<>();
-        l.add(new Movie("Star Wars", 8.7, 1977));
-        l.add(new Movie("Empire Strikes Back", 8.8, 1980));
-        l.add(new Movie("Return of the Jedi", 8.4, 1983));
-
-        // Sorting the movies based on the year using compareTo
-        Collections.sort(l);
-
-        // Printing the sorted movies
-        System.out.println("Movies after sorting by year: ");
-        for (Movie m : l) {
-            System.out.println(m.getName() + " " + m.getRating() + " " + m.getYear());
-        }
+    // Override toString method to print movie details
+    @Override
+    public String toString() {
+        return name + " (" + year + ") - Rating: " + rating;
     }
 }
+
