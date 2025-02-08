de2761ec31fa264c65980e40e8444a4a68d2e642
VenkataSaiSankeerthReddyYama <sankeerthreddy2002@gmail.com>
2025-02-08 15:10:05 +0530

    Day7

--- wipro_java_project/src/com/wipro/java/junit/test/EmployeeServiceTest.java
+++ wipro_java_project/src/com/wipro/java/junit/test/EmployeeServiceTest.java
@@ -12,14 +12,14 @@ public class EmployeeServiceTest {
     @Before
     public void setUp() {
         m = new EmployeeService();
-        m.addEmployee(new Employee(1, "Aswathi", "IT", 50000.0, LocalDate.of(2022, 1, 10)));
-        m.addEmployee(new Employee(2, "Bhava", "HR", 60000.0, LocalDate.of(2021, 5, 20)));
-        m.addEmployee(new Employee(3, "Tharini", "Finance", 70000.0, LocalDate.of(2020, 8, 15)));
+        m.addEmployee(new Employee(1, "Sankeerth", "IT", 50000.0, LocalDate.of(2022, 1, 10)));
+        m.addEmployee(new Employee(2, "Suhail", "HR", 60000.0, LocalDate.of(2021, 5, 20)));
+        m.addEmployee(new Employee(3, "Hulk", "Finance", 70000.0, LocalDate.of(2020, 8, 15)));
     }
 
     @Test
     public void testAddEmployee() {
-        Employee emp = new Employee(4, "Aswa", "IT", 55000.0, LocalDate.of(2023, 3, 5));
+        Employee emp = new Employee(4, "Batman", "IT", 55000.0, LocalDate.of(2023, 3, 5));
         m.addEmployee(emp);
         assertEquals(4, m.getAllEmployees().size());
     }
@@ -34,7 +34,7 @@ public class EmployeeServiceTest {
     public void testSearchEmployee() {
         Employee emp = m.searchEmployee(2);
         assertNotNull(emp);
-        assertEquals("Bhava", emp.getName());
+        assertEquals("Suhail", emp.getName());
     }
 
     @Test
@@ -52,15 +52,15 @@ public class EmployeeServiceTest {
     public void testFilterByDepartment() {
         List<Employee> itEmployees = m.filterByDepartment("IT");
         assertEquals(1, itEmployees.size());
-        assertEquals("Aswathi", itEmployees.get(0).getName());
+        assertEquals("Sakeerth", itEmployees.get(0).getName());
     }
 
     @Test
     public void testSortByName() {
         List<Employee> sortedEmployees = m.sortByName();
-        assertEquals("Aswathi", sortedEmployees.get(0).getName());
-        assertEquals("Bhava", sortedEmployees.get(1).getName());
-        assertEquals("Tharini", sortedEmployees.get(2).getName());
+        assertEquals("Sankeerth", sortedEmployees.get(0).getName());
+        assertEquals("Suhail", sortedEmployees.get(1).getName());
+        assertEquals("Ironman", sortedEmployees.get(2).getName());
     }
 
     @Test
